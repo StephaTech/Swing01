44852413bc0ba0c42324dad3e3b3cd7ffc80a082
StephaTech <stephanysouza.direito@gmail.com>
2020-05-31 04:43:39 +0100

    classees TextFields and Labels
    not working yet

--- src/FormPanel.java
+++ src/FormPanel.java
@@ -1,14 +1,31 @@
 import java.awt.Dimension;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.Border;
 
 public class FormPanel extends JPanel {
+	
+	private JLabel nameLabel;
+	private JLabel occupationLabel;
+	private JTextField nameField;
+	private JTextField occupationField;
+	private JButton okBtn;
+	
 	public FormPanel() {
 	Dimension dim = getPreferredSize();
 	dim.width = 250;
 	setPreferredSize(dim);//present default size
+	
+	nameLabel = new JLabel("Name");
+	occupationLabel = new JLabel("Occupation");
+	nameField = new JTextField(10);
+	occupationField = new JTextField(10);
+	
+	okBtn = new JButton("Ok");
 	
 	Border innerBorder = BorderFactory.createTitledBorder("Add Person");
 	Border outerBorder = BorderFactory.createEmptyBorder(5, 5, 5, 5);
